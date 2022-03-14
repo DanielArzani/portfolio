8154c0095b2b15aa3a79e3fd5b7a1afbfdc0e230
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-03-13 22:13:28 -0400

    delete un-needed boilerplate and create starting folders and files and install dependencies

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles/app.css";
+
+function App() {
+  return <h1>My Portfolio</h1>;
+}
+
+export default App;
---
+++ src/components/component.js
@@ -0,0 +1,8 @@
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Projects from "./Projects/Projects";
+import Resume from "./Resume/Resume";
+
+export { About, Contact, Footer, Header, Projects, Resume };
