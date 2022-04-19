8ba5653f3d9e667f258729bef02fa5285525da59
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2022-04-19 10:18:15 -0400

    Make formSubmit functional for live site

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,57 +1,57 @@
-import React from "react";
-import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import React from 'react';
+import { Box, Button, Container, TextField, Typography } from '@mui/material';
 
 function Contact() {
   return (
-    <Container sx={{ padding: "0 5rem" }}>
-      <Typography component="h3" variant="h3">
+    <Container sx={{ padding: '0 5rem' }}>
+      <Typography component='h3' variant='h3'>
         Contact Me
       </Typography>
       <Box
-        component={"form"}
-        action="https://formsubmit.co/daniel.arzanipour@gmail.com"
-        method="POST"
+        component={'form'}
+        action='5951e52cb8a41b15aaa20ab4c48c7d1f'
+        method='POST'
         sx={{
-          display: "flex",
-          alignItems: "flex-start",
-          flexDirection: "column",
-          gap: "1rem",
-          marginTop: "3rem",
-          maxWidth: "30rem",
+          display: 'flex',
+          alignItems: 'flex-start',
+          flexDirection: 'column',
+          gap: '1rem',
+          marginTop: '3rem',
+          maxWidth: '30rem',
         }}
       >
         <TextField
-          name="name"
-          id="name"
-          label="required"
-          variant="outlined"
+          name='name'
+          id='name'
+          label='required'
+          variant='outlined'
           fullWidth
-          placeholder="Daniel"
-          helperText="Your Name Please"
+          placeholder='Daniel'
+          helperText='Your Name Please'
           required
         />
         <TextField
-          name="email"
-          id="name"
-          label="required"
-          variant="outlined"
+          name='email'
+          id='name'
+          label='required'
+          variant='outlined'
           fullWidth
-          placeholder="email@gmail.com"
-          helperText="Your Email Please"
+          placeholder='email@gmail.com'
+          helperText='Your Email Please'
           required
         />
         <TextField
-          name="message"
-          id="message"
-          label="required"
-          placeholder="Press Enter for more space"
+          name='message'
+          id='message'
+          label='required'
+          placeholder='Press Enter for more space'
           multiline
           maxRows={8}
           fullWidth
-          helperText="Your Message Please"
+          helperText='Your Message Please'
           required
         />
-        <Button type="submit" variant="contained" size="large">
+        <Button type='submit' variant='contained' size='large'>
           Submit
         </Button>
       </Box>
